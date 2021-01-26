f51dc33da9312b542db82672c4346703e9f35a73
otallyto <rodrigues.tallyto@hotmail.com>
2021-01-25 20:25:38 -0400

    question 1008 solved

---
+++ 1008.py
@@ -0,0 +1,6 @@
+numeroUsuario = int(input())
+diasTrabalhos = int(input())
+valorPorHoraTrabalhada = float(input())
+salario = diasTrabalhos * valorPorHoraTrabalhada
+
+print('NUMBER = %d\nSALARY = U$ %.2f' % (numeroUsuario, salario))
